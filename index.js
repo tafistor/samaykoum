3eb2758faaa344e87a2ef37d68b4ed9c2d756d13
tafistor <mahmoud.al.hachemi@gmail.com>
2021-06-02 00:14:25 +0200

    myfirstcommet

---
+++ index.js
@@ -0,0 +1,8 @@
+function myfucntion() {
+    valuename = document.getElementById("fname").value;
+    valuelname = document.getElementById("lname").value;
+    valueemail = document.getElementById("email").value;
+    alert("your name :  " + valuename +" \n your last name : " + valuelname +"\n your email : " + valueemail );
+    
+
+}
